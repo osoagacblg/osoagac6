12950d3a331612d84196ce2fff73d33e7f46f96c
osoagacblg <akbashuseyin3883@gmail.com>
2026-05-19 21:24:41 +0300

    Add files via upload

--- tbp 6/script.js
+++ tbp 6/script.js
@@ -1,6 +1,6 @@
 const agaclar = [
     {
-        ad: "Çınar (Platanus)",
+        ad: 'Çınar (<span class="bilimsel">Platanus</span>)',
         bilgi: "10-15 yaş aralığındadır.El ayasına benzeyen, genellikle 5-7 loblu ve geniş yapraklara sahiptir. Bahar aylarında parlak yeşil bir renktedir.Çınarların en belirgin özelliklerinden biri, yaşlandıkça gövde kabuklarının levhalar halinde dökülerek alacalı (mozaik gibi) bir görünüm kazanmasıdır. Bu ağaç henüz genç olduğu için kabuk yapısı daha düzgündür.Çok hızlı büyüyen ve çok geniş bir tepe tacı oluşturabilen bir ağaçtır. Bu özelliği nedeniyle tarih boyunca şehir meydanlarında gölge ağacı olarak dikilmiştir.Kirli havaya ve zorlu şehir koşullarına son derece dayanıklıdır.",
         resim: "images/agac6.jpg"
     },
@@ -13,7 +13,7 @@ function getId() {
 
 function agaciGoster(id) {
     const agac = agaclar[id - 1];
-    document.getElementById("agacAdi").innerText = agac.ad;
+    document.getElementById("agacAdi").innerHTML = agac.ad;
     document.getElementById("agacBilgi").innerText = agac.bilgi;
     document.getElementById("agacResim").src = agac.resim;
 }
